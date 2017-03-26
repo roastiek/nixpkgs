d8301a80ac9cc2d2f52c57817d7983fd2af52063
deedrah <deedrah@home.lan>
2017-03-26 12:51:16 +0200

    Removed enchat from gedit dependecnies

--- pkgs/desktops/gnome-3/3.20/apps/gedit/default.nix
+++ pkgs/desktops/gnome-3/3.20/apps/gedit/default.nix
@@ -1,4 +1,4 @@
-{ stdenv, intltool, fetchurl, enchant, isocodes
+{ stdenv, intltool, fetchurl, isocodes
 , pkgconfig, gtk3, glib
 , bash, wrapGAppsHook, itstool, libsoup, libxml2
 , gnome3, librsvg, gdk_pixbuf, file, gspell }:
@@ -10,7 +10,7 @@ stdenv.mkDerivation rec {
 
   nativeBuildInputs = [ pkgconfig wrapGAppsHook ];
 
-  buildInputs = [ gtk3 glib intltool itstool enchant isocodes
+  buildInputs = [ gtk3 glib intltool itstool isocodes
                   gdk_pixbuf gnome3.defaultIconTheme librsvg libsoup
                   gnome3.libpeas gnome3.gtksourceview libxml2
                   gnome3.gsettings_desktop_schemas gnome3.dconf file gspell ];
