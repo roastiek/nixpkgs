bfa8b823c43087faad2702bafebaf2a84bc5254e
Rostislav Benes <rostislav.benes@firma.seznam.cz>
2020-03-17 13:35:26 +0100

    Revert "vscode: specify runtimeDependencies instead of LD_LIBRARY_PATH"
    This reverts commit d1918bb0d90ee99601b18be55bba0fd78904e2a3.

--- pkgs/applications/editors/vscode/generic.nix
+++ pkgs/applications/editors/vscode/generic.nix
@@ -50,6 +50,7 @@
 
 stdenv.mkDerivation (finalAttrs:
 let
+  inherit (stdenv.hostPlatform) system;
 
   # Vscode and variants allow for users to download and use extensions
   # which often include the usage of pre-built binaries.
@@ -113,8 +114,7 @@ in
 {
 
   inherit pname version src sourceRoot dontFixup;
-
-  passthru = {
+   passthru = {
     inherit executableName longName tests updateScript;
     fhs = fhs { };
     fhsWithPackages = f: fhs { additionalPkgs = f; };
@@ -211,7 +211,11 @@ in
       --add-flags "\''${NIXOS_OZONE_WL:+\''${WAYLAND_DISPLAY:+--ozone-platform-hint=auto --enable-features=WaylandWindowDecorations}}"
       --add-flags ${lib.escapeShellArg commandLineArgs}
     )
-  '';
+  '' + ( lib.optionalString (system == "i686-linux" || system == "x86_64-linux") ''
+    gappsWrapperArgs+=(
+      --prefix LD_LIBRARY_PATH : ${lib.makeLibraryPath [ systemd fontconfig ]}
+    )
+  '');
 
   # See https://github.com/NixOS/nixpkgs/issues/49643#issuecomment-873853897
   # linux only because of https://github.com/NixOS/nixpkgs/issues/138729
