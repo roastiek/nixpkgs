766c80c222fb7c05404231dab8f3e963650ca9ad
Rostislav Benes <r.dee.b.b@gmail.com>
2026-01-11 13:00:59 +0100

    tlp with power profile daemon

--- nixos/modules/services/hardware/tlp.nix
+++ nixos/modules/services/hardware/tlp.nix
@@ -124,12 +124,12 @@ in
         maybeDefault = val: lib.mkIf (val != null) (lib.mkDefault val);
       in
       {
-        CPU_SCALING_GOVERNOR_ON_AC = maybeDefault cfg.cpuFreqGovernor;
-        CPU_SCALING_GOVERNOR_ON_BAT = maybeDefault cfg.cpuFreqGovernor;
-        CPU_SCALING_MIN_FREQ_ON_AC = maybeDefault cfg.cpufreq.min;
-        CPU_SCALING_MAX_FREQ_ON_AC = maybeDefault cfg.cpufreq.max;
-        CPU_SCALING_MIN_FREQ_ON_BAT = maybeDefault cfg.cpufreq.min;
-        CPU_SCALING_MAX_FREQ_ON_BAT = maybeDefault cfg.cpufreq.max;
+        # CPU_SCALING_GOVERNOR_ON_AC = maybeDefault cfg.cpuFreqGovernor;
+        # CPU_SCALING_GOVERNOR_ON_BAT = maybeDefault cfg.cpuFreqGovernor;
+        # CPU_SCALING_MIN_FREQ_ON_AC = maybeDefault cfg.cpufreq.min;
+        # CPU_SCALING_MAX_FREQ_ON_AC = maybeDefault cfg.cpufreq.max;
+        # CPU_SCALING_MIN_FREQ_ON_BAT = maybeDefault cfg.cpufreq.min;
+        # CPU_SCALING_MAX_FREQ_ON_BAT = maybeDefault cfg.cpufreq.max;
       };
 
     services.udev.packages = [ cfg.package ];
